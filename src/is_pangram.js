6f1d445f357765c8119156c2ccde151c8009ca8c
Sandy <sandyvasquez9@gmail.com>
2020-12-14 17:20:24 -0800

    panagram tests

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,23 @@
+const ALPHA = 'abcdefghijklmnopqrstuvwxyz';
 const isPangram = function(text) {
+  const letterCount = {}
 
+  letters = text.split('');
+  letters.forEach((letter) => {
+    if (letterCount[letter]) {
+      letterCount[letter] +=1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  });
+
+  for (const letter of ALPHA){
+    if (!letterCount[letter]){
+      return false
+    }
+  }
+
+  return true;
 };
 
 module.exports = isPangram;
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -8,56 +8,54 @@ describe('isPangram()', () => {
   test('works with a pangram with only lower case', () => {
     // Arrange
     const text = 'the quick brown fox jumps over the lazy dog';
-
     // Act
-
+    const answer = isPangram(text);
     // Assert
+    expect(answer).toEqual(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
-
+    const text = 'abcdefghijklmnopqrstuvwxyz';
     // Act
-
+    const answer= isPangram(text);
     // Assert
+    expect(answer).toBe(true);
 
   });
 
   test("missing character 'x'", () => {
     // Arrange
-
+    const text = 'the quick brown fo jumps over the lazy dog';
     // Act
-
+    const answer = isPangram(text);
     // Assert
-
+    expect(answer).toEqual(false);
   });
 
   test('empty sentence', () => {
-    // Arrange
-
-    // Act
-
-    // Assert
-
+   // Arrange
+   const text = '';
+   // Act
+   const answer = isPangram(text);
+   // Assert
+   expect(answer).toEqual(false);
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
-
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
     // Act
-
+    const answer = isPangram(text);
     // Assert
+    expect(answer).toEqual(true);
 
   });
 
-  test('pangram with numbers', () => {
-    // Arrange
-
-    // Act
-
-    // Assert
-
-  });
-
-  // Write your own test case
+   // Arrange
+   const text = 'the quick brown fox jumps 37 feet over the lazy dog';
+   // Act
+   const answer = isPangram(text);
+   // Assert
+   expect(answer).toEqual(true);
 });
